3ae30c7edb9e1c3b52fa498b9d880d7a6fba760a
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-22 22:43:07 +0100

    Add winning conditions

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 function Square(props) {
   return(
     <button className="square" onClick={props.onClick}>
-
+      {props.value}
     </button>
   )
 }
@@ -23,9 +23,12 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      squares: squares
+      squares: squares,
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -41,7 +44,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner =  calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    }
+
 
     return (
       <div>
@@ -81,6 +92,28 @@ class Game extends React.Component {
     );
   }
 }
+
+function calculateWinner(squares) {
+  const winningLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < winningLines.length; i++) {
+    const [a, b, c] = winningLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 
 // ========================================
 
