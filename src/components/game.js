8486f7ed1503077aafd63d2db3f3eb18f89c3f64
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-10-28 19:13:38 +0000

    Add jumpTo method

--- src/components/game.js
+++ src/components/game.js
@@ -8,6 +8,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true
     };
   }
@@ -71,7 +72,16 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext
     });
   }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+   }
 }
+
+
 
 function calculateWinner(squares) {
   const winningLines = [
